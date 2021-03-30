04650913d0fc6f7825ab7f86e3224ed627b3c755
Oleg Samarin <osamarin@openwaygroup.com>
2021-03-30 11:43:50 +0300

    Deploy artifacts

--- get_versions.sh
+++ get_versions.sh
@@ -1,6 +1,6 @@
 PROJECT_VERSION=`head -1  Version.txt`
 RELEASE_VERSION=`git describe --tags --exact-match`
-if [ $? == 0 ]; then
+if [[ -n "$RELEASE_VERSION" ]]; then
   # rem Release
   RELEASE_FLAG=ON
   VERSION_SUFFIX="ow.r"
@@ -12,3 +12,4 @@ fi
 echo "::set-output name=project_ver::$PROJECT_VERSION"
 echo "::set-output name=release_flag::$RELEASE_FLAG"
 echo "::set-output name=ver_suffix::$VERSION_SUFFIX"
+echo "::set-output name=release_ver::$RELEASE_VERSION"

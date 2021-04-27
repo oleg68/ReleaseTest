1581df9e824f594279c5ad7637842413957410f6
Oleg Samarin <osamarin@openwaygroup.com>
2021-04-27 21:36:25 +0300

    Uploading with flying-ufo/action-upload-artifacts-and-release-files

--- get_versions.sh
+++ get_versions.sh
@@ -1,15 +1,17 @@
-PROJECT_VERSION=`head -1  Version.txt`
-RELEASE_VERSION=`git describe --tags --exact-match`
-if [[ -n "$RELEASE_VERSION" ]]; then
-  # rem Release
-  RELEASE_FLAG=ON
-  VERSION_SUFFIX="ow.r"
-else
-  # Intermediate build
+#!/bin/bash
+GIT_VERSION=$1
+[[ -z $GIT_VERSION ]] && GIT_VERSION=`git describe --tags`
+PROJECT_VERSION=`echo $GIT_VERSION | cut -d- -f1`
+BUILD_VERSION=`echo $GIT_VERSION | cut -d- -f2-3 --output-delimiter=.`
+GIT_CHANGE_NUM=`echo $GIT_VERSION | cut -d- -f3`
+if [[ -n "$GIT_CHANGE_NUM" ]] || [[ "$BUILD_VERSION" < "1" ]]; then
   RELEASE_FLAG=OFF
-  VERSION_SUFFIX="ow.$(git rev-parse --short HEAD)"
+else
+  RELEASE_FLAG=ON
 fi
+
 echo "::set-output name=project_ver::$PROJECT_VERSION"
+echo "::set-output name=build_ver::$BUILD_VERSION"
+echo "::set-output name=full_ver::$PROJECT_VERSION-$BUILD_VERSION"
 echo "::set-output name=release_flag::$RELEASE_FLAG"
-echo "::set-output name=ver_suffix::$VERSION_SUFFIX"
-echo "::set-output name=release_ver::$RELEASE_VERSION"
+
